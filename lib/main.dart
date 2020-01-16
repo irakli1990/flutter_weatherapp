2f3ce37d9808ab4ec16039dd28c84f63f55c9db9
Irakli Kardava <irakli.kardava@tachspeed.pl>
2020-01-16 09:32:30 +0100

    added route generator

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sunshine/backend/bloc.dart';
 import 'package:sunshine/repository/weather_repo.dart';
-import 'package:sunshine/screen/weather_screen.dart';
+import 'package:sunshine/screen/home_screen.dart';
 
 import 'config/route_generator.dart';
 
@@ -12,22 +12,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      onGenerateRoute: RouteGenerator.generateRoute,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-       resizeToAvoidBottomInset: false,
-        appBar: AppBar(
-          title: Text("SunShine"),
+        debugShowCheckedModeBanner: false,
+        onGenerateRoute: RouteGenerator.generateRoute,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
         ),
-        body: BlocProvider(
-          create: (context) => WeatherBloc(WeatherRepository()),
-              child: WeatherSearchScreen(),
-        ),
-      )
-    );
+        home: Scaffold(
+          resizeToAvoidBottomInset: false,
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text("SunShine"),
+          ),
+          body: BlocProvider(
+            create: (context) => WeatherBloc(WeatherRepository()),
+            child: HomeScreen(),
+          ),
+        ));
   }
 }
-
